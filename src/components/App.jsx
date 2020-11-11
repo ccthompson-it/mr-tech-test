b7b032eefd6c9bd1527ae649134ff446921d8c36
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-11-11 20:30:39 +1300

    basic site layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,26 @@
-import logo from '../logo.svg';
+import React, { Component, Fragment } from 'react'
 import '../styles.css';
+import tshirt from '../classic-tee.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <Fragment>
+        <div id="nav-bar"></div>
+        <div id="card-box">
+          <img id="card-image" src={tshirt} alt="plain white t-shirt" />
+          <div id="card-body">
+            <h1 id="card-header">Classic Tee</h1>
+            <h2 id="card-price">$75.00</h2>
+            <p id="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim odio ornare elit rutrum, eget maximus ante congue. Sed rutrum, velit eget dictum ornare, orci purus luctus risus, a sollicitudin turpis libero et quam. Sed mattis metus sit amet lorem sagittis efficitur. Sed accumsan dolor ac fringilla eleifend.</p>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
