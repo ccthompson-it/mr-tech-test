afb9fb97e7a0d01e99b0f1bc1241032959318fb5
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-11-11 23:30:01 +1300

    current size and current cart now tracked in state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,26 +7,49 @@ class App extends Component {
     super(props)
     this.state = {
       dropdown: false,
+      currentSize: null,
+      small: 0,
+      med: 0,
+      large: 0
     }
   }
 
   toggleDropdown = () => {
-    this.setState({dropdown: !this.state.dropdown})
+    this.setState({ dropdown: !this.state.dropdown })
+  }
+
+  handleSubmit = () => {
+    const {currentSize} = this.state
+    if (!currentSize) {
+      alert("Please Choose a Size!")
+    }
+    else {
+      this.setState({[currentSize]: this.state[currentSize] += 1})
+      this.setState({currentSize: null})
+    }
+  }
+
+  handleSize = (size) => {
+    this.setState({currentSize: size})
   }
 
   render() {
+    console.log(this.state)
+    const {dropdown, currentSize} = this.state
     return (
       <Fragment>
         <div id="nav-bar">
           <div id="dropdown">
-            <p  id="dropdown-text" className={this.state.dropdown ? 'dropdown-active' : ''}
+            <p id="dropdown-text" className={dropdown ? 'dropdown-active text-dark' : 'text-light'}
               onMouseEnter={this.toggleDropdown} onMouseLeave={this.toggleDropdown}
             >
               My Cart
             </p>
-            {this.state.dropdown && <div id="dropdown-content">Hello!</div>}
+            {dropdown && <div id="dropdown-content">Hello!</div>}
           </div>
         </div>
+
+
         <div id="card-box">
           <img id="card-image" src={tshirt} alt="plain white t-shirt" />
           <div id="card-body">
@@ -35,11 +58,11 @@ class App extends Component {
             <p className="text-light" id="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim odio ornare elit rutrum, eget maximus ante congue. Sed rutrum, velit eget dictum ornare, orci purus luctus risus, a sollicitudin turpis libero et quam. Sed mattis metus sit amet lorem sagittis efficitur. Sed accumsan dolor ac fringilla eleifend.</p>
             <p className="text-light" id="card-size">SIZE<span className="text-red">*</span><span className="text-dark"> S</span></p>
             <div id="card-buttons">
-              <button className="size-button ">S</button>
-              <button className="size-button">M</button>
-              <button className="size-button">L</button>
+              <button className={(currentSize == 'small' && 'size-active') + " size-button"} onClick={() => this.handleSize('small')}>S</button>
+              <button className={(currentSize == 'med' && 'size-active') + " size-button"} onClick={() => this.handleSize('med')}>M</button>
+              <button className={(currentSize == 'large' && 'size-active') + " size-button"} onClick={() => this.handleSize('large')}>L</button>
             </div>
-            <button id="submit-button">Add to Cart</button>
+            <button id="submit-button" onClick={this.handleSubmit}>Add to Cart</button>
           </div>
         </div>
       </Fragment>
